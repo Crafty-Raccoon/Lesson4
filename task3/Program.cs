e237616bfaa54d8f0723bbe814203387d1ddee1b
DESKTOP-QP6E4JG\Valtas <Kostoev-23@yandex.ru>
2022-10-18 10:11:46 +0300

    task3 complete

---
+++ task3/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая задаёт массив из 8 случайных элементов и выводит их на экран.
+
+//1, 2, 5, 7, 19, 6, 7, 8
+
+int[] arr = new int[8];
+for (int i = 0; i < 8; i++)
+{
+    arr[i] = new Random().Next(0, 100);
+}
+Console.WriteLine();
+for (int i = 0; i < 8; i++)
+    Console.Write($"{arr[i]} ");
+Console.WriteLine();
