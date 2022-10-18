1eeee8d07899569b5b01d6f782a16d0b3ec27167
DESKTOP-QP6E4JG\Valtas <Kostoev-23@yandex.ru>
2022-10-18 10:43:14 +0300

    task4 complete

---
+++ task4/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая из массива случайных чисел. Ищет второй максимум (число меньше максимального элемента, но больше всех остальных).
+// Постарайтесь сделать одним циклом
+//[1, 3, 5, 6, 6, 4] -> 5
+
+Random rnd = new Random();
+int x = rnd.Next(5, 10);
+
+int[] arr = new int[x];
+for (int i = 0; i < x; i++)
+{
+    arr[i] = new Random().Next(-100, 100);
+}
+
+int max = arr[0];
+int max2 = 0;
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > max)
+    {
+        max2 = max;
+        max = arr[i];
+    }
+}
+
+for (int i = 0; i < arr.Length; i++)
+{
+    Console.Write($"{arr[i]} ");
+}
+
+Console.WriteLine();
+Console.WriteLine($"Второе максимальное число массива = {max2}");
